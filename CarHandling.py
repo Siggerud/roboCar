128f57f394e87be4676e8b42c000da1d2da8b800
Siggerud <christian.siggerud@gmail.com>
2024-06-21 23:09:03 +0200

    added handling of turned off xbox controller

--- CarHandling.py
+++ CarHandling.py
@@ -34,77 +34,11 @@ class CarHandling:
 		self._pwmB.start(0)
 
 		self._controller = self._get_controller()
-
-	def _get_controller(self):
-		pygame.init()
-		pygame.joystick.init()
-
-		num_joysticks = pygame.joystick.get_count()
-		if num_joysticks > 0:
-			controller = pygame.joystick.Joystick(0)
-			controller.init()
-			print("Controller connected: ", controller.get_name())
-		else:
-			print("No controls found")
-
-		return controller
-
-	def _convert_button_press_to_speed(self, pressValue):
-		buttonMinValue = -1
-		buttonMaxValue = 1
-		pwmMinValue = 0
-		pwmMaxValue = self._pwmMax
-
-		pwmSpan = pwmMaxValue - pwmMinValue
-		buttonSpan = buttonMaxValue - buttonMinValue
-
-		valueScaled = float(pressValue - buttonMinValue) / float(buttonSpan)
-
-		valueMapped = round(pwmMinValue + (valueScaled * pwmSpan), 2)
-
-		return valueMapped
-
-	def _handle_turn_values(self):
-		turnValue = self._controller.get_hat(0)[0] # only handle the horizontal value
-		if turnValue == -1:
-			self._turnLeft = True
-			self._turnRight = False
-		elif turnValue == 1:
-			self._turnLeft = False
-			self._turnRight = True
-		elif turnValue == 0:
-			self._turnLeft = False
-			self._turnRight = False
-
-		if not self._goForward and not self._goReverse:
-			self._change_duty_cycle(self._pwmMax)
-
-	def _change_duty_cycle(self, speed):
-		print(speed)
-		self._pwmA.ChangeDutyCycle(speed)
-		self._pwmB.ChangeDutyCycle(speed)
-
-	def _handle_axis_values(self, event):
-		axis = event.axis
-		buttonPressValue = self._controller.get_axis(axis)
-
-		speed = self._convert_button_press_to_speed(buttonPressValue)
-		if speed > self._pwmTreshold: # only change speed if over the treshold
-			self._change_duty_cycle(speed)
-			if axis == 4:
-				self._goForward = True
-				self._goReverse = False
-			elif axis == 5:
-				self._goForward = False
-				self._goReverse = True
-		else:
-			self._change_duty_cycle(0)
-
-			self._goForward = False
-			self._goReverse = False
-			
-
-	def handle_xbox_input(self, threadEvent):
+        
+    def handle_xbox_input(self, threadEvent):
+        if not self._controller:
+            threadEvent.set()
+    
 		while not threadEvent.is_set():
 
 			for event in pygame.event.get():
@@ -166,6 +100,76 @@ class CarHandling:
 						GPIO.output(self._rightBackward, GPIO.HIGH)
 
 		self._cleanup()
+
+	def _get_controller(self):
+        controller = None
+    
+		pygame.init()
+		pygame.joystick.init()
+
+		num_joysticks = pygame.joystick.get_count()
+		if num_joysticks > 0:
+			controller = pygame.joystick.Joystick(0)
+			controller.init()
+			print("Controller connected: ", controller.get_name())
+		else:
+			print("No controls found. Turn on the controller")
+
+		return controller
+
+	def _convert_button_press_to_speed(self, pressValue):
+		buttonMinValue = -1
+		buttonMaxValue = 1
+		pwmMinValue = 0
+		pwmMaxValue = self._pwmMax
+
+		pwmSpan = pwmMaxValue - pwmMinValue
+		buttonSpan = buttonMaxValue - buttonMinValue
+
+		valueScaled = float(pressValue - buttonMinValue) / float(buttonSpan)
+
+		valueMapped = round(pwmMinValue + (valueScaled * pwmSpan), 2)
+
+		return valueMapped
+
+	def _handle_turn_values(self):
+		turnValue = self._controller.get_hat(0)[0] # only handle the horizontal value
+		if turnValue == -1:
+			self._turnLeft = True
+			self._turnRight = False
+		elif turnValue == 1:
+			self._turnLeft = False
+			self._turnRight = True
+		elif turnValue == 0:
+			self._turnLeft = False
+			self._turnRight = False
+
+		if not self._goForward and not self._goReverse:
+			self._change_duty_cycle(self._pwmMax)
+
+	def _change_duty_cycle(self, speed):
+		print(speed)
+		self._pwmA.ChangeDutyCycle(speed)
+		self._pwmB.ChangeDutyCycle(speed)
+
+	def _handle_axis_values(self, event):
+		axis = event.axis
+		buttonPressValue = self._controller.get_axis(axis)
+
+		speed = self._convert_button_press_to_speed(buttonPressValue)
+		if speed > self._pwmTreshold: # only change speed if over the treshold
+			self._change_duty_cycle(speed)
+			if axis == 4:
+				self._goForward = True
+				self._goReverse = False
+			elif axis == 5:
+				self._goForward = False
+				self._goReverse = True
+		else:
+			self._change_duty_cycle(0)
+
+			self._goForward = False
+			self._goReverse = False
 
 	def _cleanup(self):
 		GPIO.cleanup()
