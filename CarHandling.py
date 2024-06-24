18326c220e307957db42754e4e7890e97b09161a
Siggerud <christian.siggerud@gmail.com>
2024-06-24 20:54:04 +0200

    minor change

--- CarHandling.py
+++ CarHandling.py
@@ -212,7 +212,7 @@ class CarHandling:
 			self._turnRight = False
 
 		if not self._goForward and not self._goReverse:
-			self._change_duty_cycle([self._pwmA, self._pwmB], self._pwmMax)
+			self._change_duty_cycle([self._pwmA, self._pwmB], self._pwmMaxTT)
 
 	def _change_duty_cycle(self, pwms, speed):
 		for pwm in pwms:
