6b53217669681f42386ffc279cdfae5e05f86875
Siggerud <christian.siggerud@gmail.com>
2024-06-29 19:05:55 +0200

    added file

---
+++ Camera.py
@@ -0,0 +1,21 @@
+from picamera2 import Picamera2, Preview
+from libcamera import Transform
+from time import sleep
+
+class Camera:
+    def __init__(self, resolution, rotation=True):
+        self._cam = Picamera2
+
+        if rotation:
+            camera_config = self._cam.create_preview_configuration(main={"size": resolution}, transform=Transform(hflip=1, vflip=1))
+        else:
+            camera_config = self._cam.create_preview_configuration(main={"size": resolution})
+        self._cam.configure(camera_config)
+
+    def start_preview(self, event):
+        self._cam.start_preview(Preview.QT)  # must use this preview to run over ssh
+        self._cam.start()  # start camera
+
+        while not event.is_set():
+            sleep(0.5)
+        self._cam.close()
