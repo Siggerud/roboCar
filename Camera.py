b2f66c82bf3f5dd51d7c4884a2c8bf79d3c6a82a
Siggerud <christian.siggerud@gmail.com>
2024-07-03 19:27:56 +0200

    small change

--- Camera.py
+++ Camera.py
@@ -3,100 +3,99 @@ import os
 os.environ["LIBCAMERA_LOG_LEVELS"] = "3" #disable info and warning logging
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide" # disable pygame welcome message
 from libcamera import Transform
-from time import sleep
+from time import sleep, time
 import pygame
 
 class Camera:
-    def __init__(self, resolution, rotation=True):
-        self._cam = Picamera2()
+	def __init__(self, resolution, rotation=True):
+		self._cam = Picamera2()
 
-        if rotation:
-            camera_config = self._cam.create_preview_configuration(main={"size": resolution},
-                                                                   transform=Transform(hflip=1, vflip=1))
-        else:
-            camera_config = self._cam.create_preview_configuration(main={"size": resolution})
-        self._cam.configure(camera_config)
+		if rotation:
+			camera_config = self._cam.create_preview_configuration(main={"size": resolution}, transform=Transform(hflip=1, vflip=1))
+		else:
+			camera_config = self._cam.create_preview_configuration(main={"size": resolution})
+		self._cam.configure(camera_config)
 
-        self._lastStickValue = 0
-        self._stickValueChanged = False
-        self._minZoomValue = 1
-        self._maxZoomValue = 0.35
-        self._standardSize = None
-        self._full_res = None
 
-        self._controller = self._get_controller()
-        if not self._controller:
-            print("No controls found. Turn on the controller")
-            return
+		self._lastStickValue = 0
+		self._stickValueChanged = False
+		self._minZoomValue = 1
+		self._maxZoomValue = 0.35
 
-    def start_preview(self, threadEvent):
-        self._cam.start_preview(Preview.QT)  # must use this preview to run over ssh
-        self._cam.start()  # start camera
-        print("Starting camera preview")
+		self._controller = self._get_controller()
+		if not self._controller:
+			print("No controls found. Turn on the controller")
+			return
 
-        self._standardSize = self._cam.capture_metadata()['ScalerCrop'][2:]
-        self._full_res = self._cam.camera_properties['PixelArraySize']
 
-        while not threadEvent.is_set():
-            for event in pygame.event.get():
-                eventType = event.type
+	def start_preview(self, threadEvent):
+		self._cam.start_preview(Preview.QT)  # must use this preview to run over ssh
+		self._cam.start()  # start camera
 
-                if eventType == pygame.JOYAXISMOTION:
-                    axis = event.axis
-                    if axis == 1:
-                        buttonPressValue = self._controller.get_axis(axis)
-                        print(buttonPressValue)
-                        if buttonPressValue >= -1 and buttonPressValue <= 0:
-                            stickValue = self._round_nearest(buttonPressValue, 0.05)
+		self._standardSize = self._cam.capture_metadata()['ScalerCrop'][2:]
+		self._full_res = self._cam.camera_properties['PixelArraySize']
 
-                            if stickValue == self._lastStickValue:
-                                self._stickValueChanged = False
-                            else:
-                                self._stickValueChanged = True
-                                self._zoomValue = self._convert_button_press_to_pwm_value(stickValue,
-                                                                                          self._minZoomValue,
-                                                                                          self._maxZoomValue, 2)
+		print("Starting camera preview")
 
-                                self._lastServoStickValue = stickValue
+		sleep(2)
 
-            if self._stickValueChanged:
-                #print(self._zoomValue)
-                self._zoom()
+		while not threadEvent.is_set():
+			for event in pygame.event.get():
+				eventType = event.type
 
-        self._cam.close()
+				if eventType == pygame.JOYAXISMOTION:
+					axis = event.axis
+					if axis == 1:
+						buttonPressValue = self._controller.get_axis(axis)
+						if buttonPressValue >= -1 and buttonPressValue <= 0:
+							stickValue = self._round_nearest(buttonPressValue, 0.05)
 
-    def _zoom(self):
-        size = [int(s * self._zoomValue) for s in self._standardSize]
-        offset = [(r - s) // 2 for r, s in zip(self._full_res, size)]
-        self._cam.set_controls({"ScalerCrop": offset + size})
+							if stickValue == self._lastStickValue:
+								self._stickValueChanged = False
+							else:
+								self._stickValueChanged = True
+								self._zoomValue = self._convert_button_press_to_pwm_value(stickValue, self._minZoomValue, self._maxZoomValue, 2)
+								print(self._zoomValue)
+								self._lastServoStickValue = stickValue
 
-        self._cam.capture_metadata()  # zooms in
+			if self._stickValueChanged:
+				self._zoom()
 
-    def _round_nearest(self, x, a):
-        return round(x / a) * a
+		self._cam.close()
 
-    def _get_controller(self):
-        controller = None
+	def _zoom(self):
+		size = [int(s * self._zoomValue) for s in self._standardSize]
+		offset = [(r - s) // 2 for r, s in zip(self._full_res, size)]
+		self._cam.set_controls({"ScalerCrop": offset + size})
 
-        pygame.init()
-        pygame.joystick.init()
+		self._cam.capture_metadata()  # this zooms in
 
-        num_joysticks = pygame.joystick.get_count()
-        if num_joysticks > 0:
-            controller = pygame.joystick.Joystick(0)
-            controller.init()
-            print("Controller connected: ", controller.get_name())
+	def _round_nearest(self, x, a):
+		return round(x / a) * a
 
-        return controller
+	def _get_controller(self):
+		controller = None
 
-    def _convert_button_press_to_pwm_value(self, pressValue, pwmMinValue, pwmMaxValue, valuePrecision):
-        buttonMinValue = -1
-        buttonMaxValue = 1
+		pygame.init()
+		pygame.joystick.init()
 
-        pwmSpan = pwmMaxValue - pwmMinValue
-        buttonSpan = buttonMaxValue - buttonMinValue
+		num_joysticks = pygame.joystick.get_count()
+		if num_joysticks > 0:
+			controller = pygame.joystick.Joystick(0)
+			controller.init()
+			print("Controller connected: ", controller.get_name())
 
-        valueScaled = float(pressValue - buttonMinValue) / float(buttonSpan)
-        valueMapped = round(pwmMinValue + (valueScaled * pwmSpan), valuePrecision)
+		return controller
 
-        return valueMapped
+
+	def _convert_button_press_to_pwm_value(self, pressValue, pwmMinValue, pwmMaxValue, valuePrecision):
+		buttonMinValue = 0
+		buttonMaxValue = -1
+
+		pwmSpan = pwmMaxValue - pwmMinValue
+		buttonSpan = buttonMaxValue - buttonMinValue
+
+		valueScaled = float(pressValue - buttonMinValue) / float(buttonSpan)
+		valueMapped = round(pwmMinValue + (valueScaled * pwmSpan), valuePrecision)
+
+		return valueMapped
