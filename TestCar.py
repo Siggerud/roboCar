b02196112f14ddca19b55bdefbc9837e638631ff
Siggerud <christian.siggerud@gmail.com>
2024-08-09 19:09:06 +0200

    small change

--- TestCar.py
+++ TestCar.py
@@ -25,7 +25,7 @@ class TestCarHandling(unittest.TestCase):
             self.enB
         )
 
-        mock_setup.assert_has_calls([call(self.leftBackward), call(self.leftForward)], any_order=True)
+        mock_setup.assert_has_calls([call(self.leftBackward, GPIO.OUT), call(self.leftForward)], any_order=True)
 
 
 if __name__ == '__main__':
