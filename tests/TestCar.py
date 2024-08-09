fdf5329d3dc789d700348c9d7035438e10c54a14
Siggerud <christian.siggerud@gmail.com>
2024-08-09 18:50:22 +0200

    changed gitignore

---
+++ tests/TestCar.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import patch, call
+from CarHandling import CarHandling
+
+class TestMoterManager(unittest.TestClass):
+    # define GPIO pins
+    rightForward = 22  # IN2
+    rightBackward = 18  # IN1
+    leftForward = 16  # IN4
+    leftBackward = 15  # IN3
+    enA = 11
+    enB = 13
+
+    @patch("RPi.GPIO.setup")
+    @patch("RPi.GPIO.PWM")
+    def test_init(self, mock_output):
+
+
+        car = CarHandling(
+            self.leftBackward,
+            self.leftForward,
+            self.rightBackward,
+            self.rightForward,
+            self.enA,
+            self.enB
+        )
+
+        mock_output.assert_has_calls([call(self.leftBackward, True), call(self.leftForward, True)], any_order=True)
+
