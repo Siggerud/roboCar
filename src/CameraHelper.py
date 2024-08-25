43fcd6b102cec8b38c6a57cf5468f6768d903f51
Siggerud <christian.siggerud@gmail.com>
2024-08-25 15:50:17 +0200

    simplified carControl

--- src/CameraHelper.py
+++ src/CameraHelper.py
@@ -28,11 +28,11 @@ class CameraHelper:
         self._zoomButton = self._controlsDictCamera["Zoom"]
         self._hudButton = self._controlsDictCamera["HUD"]
 
-    def handle_xbox_input(self, button, pressValue, lock):
+    def handle_xbox_input(self, button, pressValue):
         if button == self._zoomButton:
-            self._set_zoom_value(pressValue, lock)
+            self._set_zoom_value(pressValue)
         elif button == self._hudButton and pressValue: # check that button is pushed, not released
-            self._set_hud_on_or_off(lock)
+            self._set_hud_on_or_off()
 
     def add_car(self, car):
         self._car = car
@@ -55,23 +55,23 @@ class CameraHelper:
     def get_hud_value(self):
         return self._hudActive
 
-    def update_control_values_for_video_feed(self, lock):
-        with lock:
-            if self._servo:
-                self._angleText = "Angle: " + str(self._servo.get_current_servo_angle())
+    def update_control_values_for_video_feed(self):
+        #with lock:
+        if self._servo:
+            self._angleText = "Angle: " + str(self._servo.get_current_servo_angle())
 
-            if self._car:
-                self._speedText = "Speed: " + str(self._car.get_current_speed()) + "%"
-                self._turnText = "Turn: " + self._car.get_current_turn_value()
+        if self._car:
+            self._speedText = "Speed: " + str(self._car.get_current_speed()) + "%"
+            self._turnText = "Turn: " + self._car.get_current_turn_value()
 
     def camera_buttons(self):
         return self._controlsDictCamera
 
-    def _set_hud_on_or_off(self, lock):
-        with lock:
-            self._hudActive = not self._hudActive
+    def _set_hud_on_or_off(self):
+        #with lock:
+        self._hudActive = not self._hudActive
 
-    def _set_zoom_value(self, buttonPressValue, lock):
+    def _set_zoom_value(self, buttonPressValue):
         if self._check_if_button_press_within_valid_range(buttonPressValue):
             stickValue = round(buttonPressValue, 1)
         else:
@@ -79,8 +79,8 @@ class CameraHelper:
 
         if stickValue != self._lastStickValue:
             self._lastStickValue = stickValue
-            with lock:
-                self._zoomValue = map_value_to_new_scale(stickValue, self._minZoomValue, self._maxZoomValue, 2,
+            #with lock:
+            self._zoomValue = map_value_to_new_scale(stickValue, self._minZoomValue, self._maxZoomValue, 2,
                                                      self._zoomButtonMinValue, self._zoomButtonMaxValue)
 
 
